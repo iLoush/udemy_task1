5f23acfcee3e74f609501ee61f2ef2e792786bf9
Irina <iloushkina@gmail.com>
2020-05-08 11:25:00 +0200

    task 2 completed in js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let money = prompt("What is your monthly budget?", "");
+"use strict";
+
+let money = +prompt("What is your monthly budget?", "");
 let time = prompt("Enter data in format YYYY-MM-DD", "");
 
 let appData = {
@@ -10,12 +12,80 @@ let appData = {
    savings: false
 };
 
-let userQuestion1 = prompt("Enter must expense item in this month?", ""),
-   userQuestion2 = prompt("How much is all expenses", ""),
-   userQuestion3 = prompt("Enter must expense item in this month?", ""),
-   userQuestion4 = prompt("How much is all expenses", "");
+// loop while
+/*
+let i = 0;
+while (i < 2) {
+   let a = prompt("Enter must cost name in this month?", ""),
+      b = prompt("How much is it", "");
 
-appData.expenses[userQuestion1] = userQuestion2;
-appData.expenses[userQuestion3] = userQuestion4;
+      if (typeof (a) === "string" &&
+      typeof (a) != null &&
+      typeof (b) != null &&
+      a != " " &&
+      b != " " &&
+      a.length < 50) {
+      console.log("done");
+      appData.expenses[a] = b;
+   } else {
+      console.log("Wrong");
+      i--;
+   }
+   i++;
+}
+*/
 
-alert(appData.budget / 30);
+// loop do ... while
+/*
+let i = 0;
+do {
+   let a = prompt("Enter must cost name in this month?", ""),
+      b = prompt("How much is it", "");
+
+      if (typeof (a) === "string" &&
+      typeof (a) != null &&
+      typeof (b) != null &&
+      a != " " &&
+      b != " " &&
+      a.length < 50) {
+      console.log("done");
+      appData.expenses[a] = b;
+   } else {
+      console.log("Wrong");
+      i--;
+   }
+   i++;
+} while(i<2);
+*/
+
+for (let i = 0; i < 2; i++) {
+   let a = prompt("Enter must cost name in this month?", ""),
+      b = prompt("How much is it", "");
+
+   if (typeof (a) === "string" &&
+      typeof (a) != null &&
+      typeof (b) != null &&
+      a != " " &&
+      b != " " &&
+      a.length < 50) {
+      console.log("done");
+      appData.expenses[a] = b;
+   } else {
+      console.log("Wrong");
+      i--;
+   }
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert(`Daily budget is: ${appData.moneyPerDay} eur`);
+
+if (appData.moneyPerDay < 1000) {
+   console.log("Mininum level");
+} else if (appData.moneyPerDay > 1000 && appData.moneyPerDay < 2000) {
+   console.log("Middle level");
+} else if (appData.moneyPerDay > 2000) {
+   console.log("High level");
+} else {
+   console.log("Something went wrong");
+}
